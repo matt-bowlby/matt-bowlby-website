79909a38740cc3638ee0e99d4cfb2a265f82f04d
matt-bowlby <mbowlby794@gmail.com>
2026-01-14 23:28:20 -0800

    Add basic placeholder text to main website page

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,21 @@
 </script>
 
 <main>
-	<h1>This website is really cool</h1>
+    <h1>This is the future home of Matt Bowlby's Website!</h1>
+    <p>
+        For now, you can see my projects on <a
+            href="https://github.com/matt-bowlby"
+            target="_blank"
+            rel="noopener noreferrer">GitHub</a
+        >
+        or check me out on
+        <a href="https://linkedin.com/in/matt-bowlby" target="_blank" rel="noopener noreferrer"
+            >LinkedIn</a
+        >.
+    </p>
+    <p>
+        Feel free to reach out at <a href="mailto:hello@matt-bowlby.com">hello@matt-bowlby.com</a>.
+    </p>
 </main>
 
 <style>
